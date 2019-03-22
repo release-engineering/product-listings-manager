3545985a4991eba23ea9f01388b23fc92b6e19ff
Haibo Lin <hlin@redhat.com>
2019-03-22 14:28:06 +0800

    Modulize configuration

--- product_listings_manager/app.py
+++ product_listings_manager/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify
 
 from product_listings_manager import root, rest_api_v1
-from product_listings_manager import products
+from product_listings_manager.config import load_config
 from product_listings_manager.logger import init_logging
 from product_listings_manager.models import db
 
@@ -13,27 +13,11 @@ def page_not_found_error(ex):
 def create_app():
     app = Flask(__name__)
 
+    load_config(app)
     init_logging(app)
-    app.register_error_handler(404, page_not_found_error)
-
-    # Set products.py's DB values from our Flask config:
-    app.config.from_object('product_listings_manager.config')
-    app.config.from_pyfile('/etc/product-listings-manager/config.py', silent=True)
-    app.config.from_envvar('PLM_CONFIG_FILE', silent=True)
-    products.dbname = app.config['DBNAME']  # eg. "compose"
-    products.dbhost = app.config['DBHOST']  # eg "db.example.com"
-    products.dbuser = app.config['DBUSER']  # eg. "myuser"
-    products.dbpasswd = app.config['DBPASSWD']  # eg. "mypassword"
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{}:{}@{}/{}'.format(
-        app.config['DBUSER'], app.config['DBPASSWD'], app.config['DBHOST'], app.config['DBNAME'])
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_ECHO'] = False
-    if app.debug:
-        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-        app.config['SQLALCHEMY_ECHO'] = True
-
     db.init_app(app)
 
+    app.register_error_handler(404, page_not_found_error)
     app.register_blueprint(root.blueprint, url_prefix='/')
     app.register_blueprint(rest_api_v1.blueprint, url_prefix='/api/v1.0')
     return app
