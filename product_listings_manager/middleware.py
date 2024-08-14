f7e3a9f67972c63e2467a7486a37efa7c1b27f77
Lukas Holecek <hluk@email.cz>
2024-08-14 15:03:37 +0200

    Add Strict-Transport-Security response header by default
    JIRA: RHELWF-11222

--- product_listings_manager/middleware.py
+++ product_listings_manager/middleware.py
@@ -1,11 +1,28 @@
 # SPDX-License-Identifier: GPL-2.0+
 import re
 
+from fastapi import Request
 from starlette.datastructures import URL
+from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.responses import RedirectResponse
 from starlette.types import ASGIApp, Receive, Scope, Send
 
 repeated_quotes = re.compile(r"//+")
+
+
+class AddResponseHeaders(BaseHTTPMiddleware):
+    """
+    Adds custom response headers.
+    """
+
+    def __init__(self, app: ASGIApp, headers: dict[str, str]) -> None:
+        super().__init__(app)
+        self.headers = headers
+
+    async def dispatch(self, request: Request, call_next):
+        response = await call_next(request)
+        response.headers.update(self.headers)
+        return response
 
 
 class UrlRedirectMiddleware:
